0085b7da16980babe4201c395438f6a78358d494
luisfelipesd <felipe_0411@hotmail.com>
2015-11-27 10:57:00 -0500

    pestañas por categorias

---
+++ Controllers/subcategoriasApiController.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    public class subcategoriasApiController : ApiController
+    {
+        private Model db = new Model();
+
+        // GET: api/subcategoriasApi
+        public IQueryable<categorias> Getcategorias()
+        {
+            return db.categorias;
+        }
+
+        // GET: api/subcategoriasApi/5       
+        public IQueryable<categorias> Getcategorias(int id)
+        {
+            return db.categorias.Where(c => c.id_padre == id);
+        }
+
+        // PUT: api/subcategoriasApi/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> Putcategorias(int id, categorias categorias)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != categorias.id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(categorias).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!categoriasExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/subcategoriasApi
+        [ResponseType(typeof(categorias))]
+        public async Task<IHttpActionResult> Postcategorias(categorias categorias)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.categorias.Add(categorias);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = categorias.id }, categorias);
+        }
+
+        // DELETE: api/subcategoriasApi/5
+        [ResponseType(typeof(categorias))]
+        public async Task<IHttpActionResult> Deletecategorias(int id)
+        {
+            categorias categorias = await db.categorias.FindAsync(id);
+            if (categorias == null)
+            {
+                return NotFound();
+            }
+
+            db.categorias.Remove(categorias);
+            await db.SaveChangesAsync();
+
+            return Ok(categorias);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool categoriasExists(int id)
+        {
+            return db.categorias.Count(e => e.id == id) > 0;
+        }
+    }
+}
--- js/projectJs.js
+++ js/projectJs.js
@@ -1,9 +1,17 @@
 ﻿$(function projectJs() {
-	$.getJSON(location.origin + "/api/categoriasApi", null, function (data) {
+    $.getJSON(location.origin + "/api/categoriasApi", null, function (data) {
 
-		for (var x in data) {
-			$('#category').append('<option value="' + data['id'] + '">' + data.id_padre + '</option>');
-		}
-
-	});
+        data.forEach(function (value, index) {
+            var tabid = value.id;
+            $("#tabsheader").append('<li><a href="#tab' + tabid + '" role="tab" data-toggle="tab"> <i class="fa fa-user pr-5"> </i> ' + value.nombre + '</a></li>');
+            $("#tabcontent").append('<div class="tab-pane fade in" id="tab' + tabid +'"><div class="row"><div class="col-md-12">'
+                +'<div class="panel panel-default"><div class="panel-heading">Seleccione una submétrica para evaluar</div><div class="panel-body">'
+                + '<select class="form-control" id="select' + tabid + '"></select></div></div></div></div></div>');
+            $.getJSON(location.origin + "/api/subcategoriasApi/" + tabid, null, function (datasub) {
+                datasub.forEach(function (value, index) { 
+                    $("#select"+tabid).append('<option value="'+value.id+'">'+value.nombre+'</option>')
+                });
+            });
+        });
+    });
 })
